d8015df0bcaf54e4c148cec73932f622501362f9
Yulia <juliam2007@mail.ru>
2018-01-12 13:49:25 +0300

    Code refactoring

--- js/stat.js
+++ js/stat.js
@@ -1,22 +1,63 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  var startX = 100;
+  var startY = 10;
+  var widthRect = 420;
+  var heightRect = 270;
+  var lenghtShadow = 10;
+  var margin = 40;
 
-  // Рисует тень облака
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+  var colorRect = ['rgba(0, 0, 0, 0.7)', 'rgb(256, 256, 256)'];
+  var text = ['Ура вы победили!', 'Список результатов: '];
 
-  // Рисует облако
-  ctx.fillStyle = '#ffffff';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
+  drawRect(startX + lenghtShadow, startY + lenghtShadow, widthRect, heightRect, colorRect[0]);
+  drawRect(startX, startY, widthRect, heightRect, colorRect[1]);
+  writeText(text);
+  drawHistogram(times, names);
+
+  // Рисуем прямоугольник
+  function drawRect(axisX, axisY, width, height, fillColor) {
+    ctx.fillStyle = fillColor;
+    ctx.fillRect(axisX, axisY, width, height);
+  }
 
   // Выводим надпись на облаке
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов: ', 120, 60);
+  function writeText(textArray) {
+    ctx.fillStyle = '#000';
+    ctx.font = '16px PT Mono';
+    for (var i = 0; i < textArray.length; i++) {
+      ctx.fillText(textArray[i], startX + margin, startY + (i + 1) * 25);
+    }
+  }
 
+  // Рисуем гистограмму
+  function drawHistogram(arrayTimes, arrayNames) {
+    var barWidth = 40;
+    var indent = 90;
+    var indentName = 20;
+    var indentTime = 10;
+    var histogramHeight = 150;
+    var paddingTop = 60;
+    var step = histogramHeight / (getMaxValue(times) - 0);
+
+    var initialX = startX + margin;
+    var initialY = startY + histogramHeight + indentName + indentTime + paddingTop;
+
+    for (var i = 0; i < arrayTimes.length; i++) {
+      var barHeight = arrayTimes[i] * step;
+      var getY = initialY - arrayTimes[i] * step;
+      var getX = initialX + indent * i;
+
+      ctx.fillStyle = fillBarColor(names[i]);
+      ctx.fillRect(getX, getY, barWidth, barHeight);
+
+      ctx.fillText(arrayNames[i], getX, initialY + indentName);
+      ctx.fillText(arrayTimes[i].toFixed(0), getX, getY - indentTime);
+    }
+  }
+
+  // Вспомогательные функции
   // Ищем наихудший результат
   function getMaxValue(array) {
     var max = -1;
@@ -29,17 +70,6 @@ window.renderStatistics = function (ctx, names, times) {
     return max;
   }
 
-  // Рисуем гистограмму
-  var barWidth = 40;
-  var initialX = 120;
-  var initialY = 245;
-  var indent = 90;
-  var indentName = 20;
-  var indentTime = 15;
-
-  var histogramHeight = 150;
-  var step = histogramHeight / (getMaxValue(times) - 0);
-
   // Вычисляем цвет бара в зависимости от имени игрока
   function fillBarColor(namePlayer) {
     var randomOpacity = Math.random().toFixed(2);
@@ -48,17 +78,5 @@ window.renderStatistics = function (ctx, names, times) {
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + randomOpacity + ')';
     }
-  }
-
-  for (var i = 0; i < times.length; i++) {
-    var barHeight = times[i] * step;
-    var getY = initialY - times[i] * step;
-    var getX = initialX + indent * i;
-
-    ctx.fillStyle = fillBarColor(names[i]);
-    ctx.fillRect(getX, getY, barWidth, barHeight);
-
-    ctx.fillText(names[i], getX, initialY + indentName);
-    ctx.fillText(times[i].toFixed(0), getX, getY - indentTime);
   }
 };
