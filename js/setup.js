b723321113e0d003d4adefdf9ef3fac2ecd85706
cybersunt <juliam2007@mail.ru>
2018-01-14 22:56:37 +0300

    Clone and insert template

--- js/setup.js
+++ js/setup.js
@@ -5,3 +5,13 @@ var userDialog = document.querySelector('.setup');
 var similarWizards = document.querySelector('.setup-similar');
 userDialog.classList.remove('hidden');
 similarWizards.classList.remove('hidden');
+
+// Открываем окно с похожими персонажами
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+for (var i = 0; i < 4; i++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  similarListElement.appendChild(wizardElement);
+}
