2ce2f497b9ad732fc66e3a3dcf21891bbc79cf24
cybersunt <juliam2007@mail.ru>
2018-01-14 22:52:12 +0300

    Open popup

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,7 @@
 'use strict';
 
-var setup = document.querySelector(.setup);
+// Открываем окно с похожими персонажами
+var userDialog = document.querySelector('.setup');
+var similarWizards = document.querySelector('.setup-similar');
+userDialog.classList.remove('hidden');
+similarWizards.classList.remove('hidden');
